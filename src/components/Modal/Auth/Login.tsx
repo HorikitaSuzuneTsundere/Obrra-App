82f5da30c439545b9fb463df6f01c79fbdb9659f
EJ Masaga <e.masaga.476136@umindanao.edu.ph>
2023-12-08 11:28:14 +0800

    error fixed

--- src/components/Modal/Auth/Login.tsx
+++ src/components/Modal/Auth/Login.tsx
@@ -6,7 +6,7 @@ import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { auth } from "../../../firebase/clientApp";
 import { FIREBASE_ERRORS } from '../../../firebase/errors';
 
-type LoginProps = {};
+type LoginProps = NonNullable<unknown>;
 
 const Login:React.FC<LoginProps> = () => {
   const setAuthModalState = useSetRecoilState(authModalState);
@@ -14,7 +14,7 @@ const Login:React.FC<LoginProps> = () => {
     email: "",
     password: "",
   });
-  const [signInWithEmailAndPassword, user, loading, error] =
+  const [signInWithEmailAndPassword,, loading, error] =
     useSignInWithEmailAndPassword(auth);
 
   // Firebase logic
